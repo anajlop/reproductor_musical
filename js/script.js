e4a497a7c72214ceb81468e827cd4432dcd1d920
Ana Lopez <220831@mail.unacar.mx>
2024-04-24 01:51:28 -0600

    estructura para empezar

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const canciones = [
+    {
+        titulo: "Cancion",
+        album: "Álbum",
+        artista: "Artista",
+        duración: "0:00",
+        imagen: "images/.jpg",
+        audio: "images/musica/.mp3",
+    },{
+        titulo: "Cancion",
+        album: "Álbum",
+        artista: "Artista",
+        duración: "0:00",
+        imagen: "images/.jpg",
+        audio: "images/musica/.mp3",
+    }
+];
+
+class reproductor {
+    constructor(musica){
+        this.musica = musica;
+    }
+}
